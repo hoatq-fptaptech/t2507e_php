11b8013a688e84f2b94ec7b2c4d2f9c3e0ee4f7c
quanghoatrinh <quanghoa.trinh@gmail.com>
2025-11-26 19:32:50 +0700

    header

--- api/v1/utils/helpers.php
+++ api/v1/utils/helpers.php
@@ -1,6 +1,11 @@
 <?php
 
 function sendJsonResponse($data=[], $message="Successfully", $status_code = 200){
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+    header("Content-Type: application/json; charset=utf-8");
+
     http_response_code($status_code);
     echo json_encode([
             "data"=>$data,
@@ -11,6 +16,10 @@ function sendJsonResponse($data=[], $message="Successfully", $status_code = 200)
     exit();
 }
 function sendErrorResponse($message="Fail", $status_code = 422){
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+    header("Content-Type: application/json; charset=utf-8");
     http_response_code($status_code);
     echo json_encode([
             "message"=> $message,
